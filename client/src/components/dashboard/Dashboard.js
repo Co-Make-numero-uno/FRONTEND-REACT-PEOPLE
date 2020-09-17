83f19cd50bbbed06d43799c285d93bf84ecd362b
Joshua Gralewski <jgralews3@yahoo.com>
2020-09-16 20:03:28 -0500

    Dashboard map and onClick push to issue/id working

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import IssueCard from './IssueCard'
 
-const dashboard = () => {
+const dummyData = [{
+    name: "Pothole",
+    city: "Chicago",
+    state: "IL",
+    description: "On 45th and Western",
+    id: "1"
+},
+{
+    name: "Low Water Pressure",
+    city: "Hammond",
+    state: "IN",
+    description: "3336 165th Street",
+    id: "2"
+}]
+
+const Dashboard = () => {
     return (
         <div>
-            
+            {dummyData.map(issue=><IssueCard key={issue.id} issue={issue}/>)}
         </div>
     );
 };
 
-export default dashboard;
+export default Dashboard;
--- client/src/components/dashboard/IssueCard.js
+++ client/src/components/dashboard/IssueCard.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom'
 
-const issueCard = () => {
+const IssueCard = (props) => {
+    console.log(props.issue.id)
+
+    const {push} = useHistory()
+    const clickIssue = (issue) => {
+        push(`/issue/${issue}`);
+        console.log(issue)
+    }
+
     return (
-        <div>
-            
+        <div onClick={() => clickIssue(props.issue.id)}>
+            <p>Name: {props.issue.name}</p>
+            <p>Location: {props.issue.city}, {props.issue.state}</p>
+            <p>Description: {props.issue.description}</p>
         </div>
     );
 };
 
-export default issueCard;
+export default IssueCard;
