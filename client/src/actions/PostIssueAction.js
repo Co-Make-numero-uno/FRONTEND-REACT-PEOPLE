76fa1780049641f37c034cd6c2c9c5d0c7200a46
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-19 19:08:53 -0400

    Made action creator to post new issues

--- client/src/actions/PostIssueAction.js
+++ client/src/actions/PostIssueAction.js
@@ -5,15 +5,15 @@ export const NEW_ISSUE_SUCCESS = "NEW_ISSUE_SUCCESS";
 export const NEW_ISSUE_START = "NEW_ISSUE_START";
 
 export const newIssue = (issue) => (dispatch) => {
-    console.log("New issue payload: ", issue)
     dispatch({type: NEW_ISSUE_START})
-    axios.post("http://", issue)
+    axios.post("https://co-make-back-end.herokuapp.com/issues", issue)
     .then((res)=>{
-        console.log("Posting Success: ", res);
+        console.log(res)
         dispatch({type: NEW_ISSUE_SUCCESS, payload: res.data})
     })
     .catch(err=> {
         console.log("Posting error: ", err);
-        dispatch({type: NEW_ISSUE_FAIL})
+        const errMessage = err.response.data.message;
+        dispatch({type: NEW_ISSUE_FAIL, payload: {message: errMessage, err: err}})
     })
 }
