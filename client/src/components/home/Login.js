f03c9bc000766f30b746ea2e3fd5c823269615c4
jayharville <jayharville@aol.com>
2020-09-23 12:14:45 -0700

    Completed MVP

--- client/src/components/home/Login.js
+++ client/src/components/home/Login.js
@@ -1,74 +1,160 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// import styled from 'styled-components'npm 
+import React, { useState, useEffect } from "react";
+import * as Yup from "yup";
+import axios from "axios";
 
+export default function Form({users, setUsers}) {
 
-const defaultUserState = {
-  email: '',
-  password: '',
-}
+    //this is the react state
+    const defaultState = {
+        name: "",
+        email: "",
+        password: "",
+        terms: false 
+    }
 
-const Login = () => {
-    const [user, setUser] = useState(defaultUserState);
+    const [formState, setFormState] = useState(defaultState);
+    const [errors, setErrors] = useState({ ...defaultState, terms: "" });
+    const [buttonDisabled, setButtonDisabled] = useState(true);
     
-    // const handleChange = event => {
-    //     setUser({ ...user, [event.target.name]: event.target.value });
-    //   };
-    
-      // const handleSubmit = event => {
-      //   event.preventDefault();
-      //   console.log(user.email);
-      //   console.log(user.password);
-      // };
 
+    //this is the formState schema
 
-      const handleSubmit = e => {
+    let formSchema = Yup.object().shape({
+        name: Yup.string().required("Please provide name."),
+        email: Yup
+          .string()
+          .required("Please provide a email.")
+          .email("This is not a valid email."),
+        password: Yup
+          .string()
+          .required("Please enter a correct Password")
+          .min(6, "Passwords must be at least 6 characters long."),
+        terms: Yup
+          .boolean()
+          .oneOf([true], "Please agree to the terms and conditions")
+      });
+
+      useEffect(() => {
+    //    one way is 
+    //     formSchema.isValid(formState).then(valid => {
+    //       setButtonDisabled(!valid);
+    //     });
+            if (formState.terms) {
+                setButtonDisabled(!formState.terms);
+            }
+      }, [formState]);
+
+    //this is use for the onsubmit function
+      const formSubmit = e => {
         e.preventDefault();
-        console.log('form Submitted:', user);
-        axios.post("https://co-make-back-end.herokuapp.com/users/login", user)
+        //I added axios data here so it does not fire a lot when its outside
+        console.log("Form Submitted");
+        // to reset form 
+        setFormState({...errors});
+        // console.log(formState.name)
+        // console.log(formState.email);
+        // console.log(formState.password);
+        axios
+            .post("https://co-make-back-end.herokuapp.com/users/login", formState)
             .then(res => {
-                console.log('res:', res.data);
-                setUser(defaultUserState);
-                // history.push('/')
+                console.log("form submitted success", res)
+                //I set setUser here so it can retrieve the user data to the DOM
+                setUsers(res.data);
+                //this one is to add all the user 
+                // setUsers([...users, formState]);
+                console.log("success", users)
+                setFormState({
+                    name: "",
+                    email:"",
+                    password: ""                
+                })
             })
-            .catch(err => console.log(err));
-    }
+            .catch(err => {
+                console.log("This is the Error", err)
+            });
+    };
 
+
+    //this is for the onchange function 
     const handleChange = e => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
+        const valueCheck = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        e.persist();
+    // yup.reach will allow us to "reach" into the schema and test only one part.
+    // We give reach the schema as the first argument, and the key we want to test as the second.
+        Yup
+            .reach(formSchema, e.target.name)
+    //we can then run validate using the value
+            .validate(e.target.value)
+    // if the validation is successful, we can clear the error message
+            .then(valid => {
+                setErrors({ ...errors, [e.target.name]: ""
+            });
         })
-        // if (e.target.name === 'email' || e.target.name === 'password') {
-        //     validate(e);
-        // }
-    }
-      
+    /* if the validation is unsuccessful, we can set the error message to the message 
+        returned from yup (that we created in our schema) */
+            .catch(err => {
+                setErrors({ ...errors, [e.target.name]: err.errors[0]
+            });
+        });
 
+    // Wether or not our validation was successful, we will still set the state to the new value as the user is typing
+        setFormState({
+             ...formState,
+         [e.target.name]: valueCheck
+        });
+    };
+
+    
     return (
-        <div>
-            <h1>Login Here</h1>
-            <form className="login-form" onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="text"
-            name="email"
-            value={user.email} onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="text"
-            name="password"
-            value={user.password} onChange={handleChange}
-          />
-        </label>
-        <button>Submit!</button>
-      </form>   
+        
+        <div className="form">
+            <form onSubmit={formSubmit}>
+                <h1>LOGIN</h1>
+            {/* <label htmlFor="name">
+                Name
+                <input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    value={formState.name}
+                    label="Name"
+                    errors={errors}
+                 />
+                 {errors.name.length !== 0 && <p className="error">{errors.name}</p>}
+            </label> */}
+            <label htmlFor="email">
+                Email
+                <input className="email"
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={formState.email}
+                    label="Email"
+                    errors={errors}
+                />
+                {errors.email.length !== 0 && <p className="error">{errors.email}</p>}
+            </label>
+            <label htmlFor="password">
+                Password
+                <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={formState.password}
+                    label="Password"
+                    errors={errors}
+                />
+                {errors.password.length !== 0 && <p className="error">{errors.password}</p>}
+            </label>
+            <label className="terms" htmlFor="terms">
+                <input name="terms" type="checkbox" onChange={handleChange}/>
+                Terms of Service
+            </label>
+            <button disabled={buttonDisabled}>SUBMIT</button>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
+        </form>
+        
         </div>
-    );
-};
-
-export default Login;
+        
+    )
+}
--- client/src/components/home/Signup.js
+++ client/src/components/home/Signup.js
@@ -1,11 +1,187 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import * as Yup from "yup";
+import axios from "axios";
+export default function Form({users, setUsers}) {
 
-const Signup = () => {
+    //this is the react state
+    const defaultState = {
+        name: "",
+        email: "",
+        password: "",
+        city: "",
+        state: ""
+    }
+
+    const [formState, setFormState] = useState(defaultState);
+    const [errors, setErrors] = useState({ ...defaultState, terms: "" });
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    
+
+    //this is the formState schema
+
+    let formSchema = Yup.object().shape({
+        name: Yup.string().required("Please provide name."),
+        email: Yup
+          .string()
+          .required("Please provide a email.")
+          .email("This is not a valid email."),
+        password: Yup
+          .string()
+          .required("Please enter a correct Password")
+          .min(6, "Passwords must be at least 6 characters long."),
+        city: Yup.string().required("Please provide name."),
+        state: Yup.string().required("Please provide name."),
+        terms: Yup
+          .boolean()
+          .oneOf([true], "Please agree to the terms and conditions")
+      });
+
+      useEffect(() => {
+    //    one way is 
+    //     formSchema.isValid(formState).then(valid => {
+    //       setButtonDisabled(!valid);
+    //     });
+            if (formState.terms) {
+                setButtonDisabled(!formState.terms);
+            }
+      }, [formState]);
+
+    //this is use for the onsubmit function
+      const formSubmit = e => {
+        e.preventDefault();
+        //I added axios data here so it does not fire a lot when its outside
+        console.log("Form Submitted");
+        // to reset form 
+        setFormState({...errors});
+        // console.log(formState.name)
+        // console.log(formState.email);
+        // console.log(formState.password);
+        axios
+            .post("https://co-make-back-end.herokuapp.com/users/register", formState)
+            .then(res => {
+                console.log("form submitted success", res)
+                //I set setUser here so it can retrieve the user data to the DOM
+                setUsers(res.data);
+                //this one is to add all the user 
+                // setUsers([...users, formState]);
+                console.log("success", users)
+                setFormState({
+                    name: "",
+                    email:"",
+                    password: "",
+                    city: "",
+                    state: ""               
+                })
+            })
+            .catch(err => {
+                console.log("This is the Error", err)
+            });
+    };
+
+
+    //this is for the onchange function 
+    const handleChange = e => {
+        const valueCheck = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        e.persist();
+    // yup.reach will allow us to "reach" into the schema and test only one part.
+    // We give reach the schema as the first argument, and the key we want to test as the second.
+        Yup
+            .reach(formSchema, e.target.name)
+    //we can then run validate using the value
+            .validate(e.target.value)
+    // if the validation is successful, we can clear the error message
+            .then(valid => {
+                setErrors({ ...errors, [e.target.name]: ""
+            });
+        })
+    /* if the validation is unsuccessful, we can set the error message to the message 
+        returned from yup (that we created in our schema) */
+            .catch(err => {
+                setErrors({ ...errors, [e.target.name]: err.errors[0]
+            });
+        });
+
+    // Wether or not our validation was successful, we will still set the state to the new value as the user is typing
+        setFormState({
+             ...formState,
+         [e.target.name]: valueCheck
+        });
+    };
+
+    
     return (
-        <div>
-            
+        
+        <div className="form">
+            <form onSubmit={formSubmit}>
+                <h1>SIGN UP</h1>
+            <label htmlFor="name">
+                Full Name
+                <input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    value={formState.name}
+                    label="Name"
+                    errors={errors}
+                 />
+                 {errors.name.length !== 0 && <p className="error">{errors.name}</p>}
+            </label>
+            <label htmlFor="email">
+                Email
+                <input className="email"
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={formState.email}
+                    label="Email"
+                    errors={errors}
+                />
+                {errors.email.length !== 0 && <p className="error">{errors.email}</p>}
+            </label>
+            <label htmlFor="password">
+                Password
+                <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={formState.password}
+                    label="Password"
+                    errors={errors}
+                />
+                {errors.password.length !== 0 && <p className="error">{errors.password}</p>}
+            </label>
+            <label htmlFor="city">
+                City
+                <input className="city"
+                    type="text"
+                    name="city"
+                    onChange={handleChange}
+                    value={formState.city}
+                    label="city"
+                    errors={errors}
+                 />
+                 {errors.city.length !== 0 && <p className="error">{errors.city}</p>}
+            </label>
+            <label htmlFor="state">
+                State
+                <input className="email"
+                    type="text"
+                    name="state"
+                    onChange={handleChange}
+                    value={formState.state}
+                    label="state"
+                    errors={errors}
+                 />
+                 {errors.state.length !== 0 && <p className="error">{errors.state}</p>}
+            </label>
+            <label className="terms" htmlFor="terms">
+                <input name="terms" type="checkbox" onChange={handleChange}/>
+                Terms of Service
+            </label>
+            <button disabled={buttonDisabled}>SUBMIT</button>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
+        </form>
         </div>
-    );
-};
-
-export default Signup;
+        
+    )
+}
